0c82b23dbec96e5043900886668199f84dd2371d
RajiiRajkumar <rajirk2022@gmail.com>
2024-09-04 14:48:28 +0530

    this is my second commit

---
+++ C2TC/src/com/tns/day2/datatype.java
@@ -0,0 +1,24 @@
+package com.tns.day2;
+
+public class datatype {
+
+	public static void main(String[] args) {
+
+			 System.out.println("Datatype\t Min.Value\t Max.Value\t"); 
+
+			 System.out.println("Byte\t\t"+ Byte.MIN_VALUE + "\t\t" + Byte.MAX_VALUE); 
+
+			 System.out.println("Short\t\t"+ Short.MIN_VALUE + "\t\t\t" + Short.MAX_VALUE); 
+
+	         System.out.println("Integer\t"+ Integer.MIN_VALUE + "\t\t" + Integer.MAX_VALUE); 
+
+	         System.out.println("Float\t\t"+ Float.MIN_VALUE + "\t\t\t" + Float.MAX_VALUE); 
+
+	         System.out.println("Long\t\t"+ Long.MIN_VALUE + "\t"+ Long.MAX_VALUE); 
+
+	         System.out.println("Double\t"+ Double.MIN_VALUE + "\t\t" + Short.MAX_VALUE); 
+
+	         System.out.println("Character\t"+ Character.MIN_VALUE + "\t\t" + Character.MAX_VALUE); 
+       }
+
+}
